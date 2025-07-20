573062a9ee2ff55647d27947b9edecdf62547a89
khaliuss <khalidkhushmadov@gmail.com>
2025-07-20 16:33:20 +0300

    Guess Star with repeating bug

--- GuessStar/app/src/main/java/com/example/guessstar/MainActivity.java
+++ GuessStar/app/src/main/java/com/example/guessstar/MainActivity.java
@@ -1,63 +1,224 @@
 package com.example.guessstar;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.print.PrintAttributes;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
+    String urlSite = "https://www.posh24.se/kandisar";
+    ImageView imageViewPicture;
+    Button button1;
+    Button button2;
+    Button button3;
+
+    ArrayList<String> urls;
+    ArrayList<String> names;
+
+    private int numberOfQuestion;
+    private int numberOfRightAnswer;
+    private ArrayList<Button> buttons;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
-        String nameString  = "Alex,Drake,Bob,Joshea";
-        String[] names = nameString.split(",");
-        for (String name:names){
-            Log.i("MyName",name);
-        }*/
+        imageViewPicture = findViewById(R.id.imageView);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        buttons = new ArrayList<>();
 
-        /*
-        String geometry = "Geometry";
-        String meter = geometry.substring(3,8);
-        Log.i("MyName",meter);
+        buttons.add(button1);
+        buttons.add(button2);
+        buttons.add(button3);
 
 
-        String river = "Mississippi";
-        Pattern pattern = Pattern.compile("Mi(.*?)pi");
-        Matcher matcher = pattern.matcher(river);
-
-        while (matcher.find()){
-            Log.i("MyName",matcher.group(1));
-        }
-        */
 
 
-        String url = "<div class=\"image\">\n" +
-                "\t\t\t\t\t\t<img decoding=\"async\" src=\"https://www.posh24.se/files/cdn-sub/images/-profile/02ac24d552f2499528ea43eb61685ec7b.jpg\" alt=\"Adam Alsing\"/>\n";
+        urls = new ArrayList<>();
+        names = new ArrayList<>();
 
-        Pattern imgPattern = Pattern.compile("src=\"(.*?)\"");
-        Pattern namePattern = Pattern.compile("alt=\"(.*?)\"");
 
-        Matcher nameMatcher = namePattern.matcher(url);
-        Matcher imgMatcher = imgPattern.matcher(url);
+        getContent();
+        playGame();
 
-        while (nameMatcher.find()){
-            Log.i("MyName",nameMatcher.group(1));
-        }
 
-        while (imgMatcher.find()){
-            Log.i("MyName",imgMatcher.group(1));
-        }
 
 
     }
+
+    private void playGame(){
+        generateQuestion();
+
+        DownloadImages task = new DownloadImages();
+        try {
+            Bitmap bitmap = task.execute(urls.get(numberOfQuestion)).get();
+            if (bitmap != null){
+                imageViewPicture.setImageBitmap(bitmap);
+                for (int i = 0; i < buttons.size(); i++) {
+                    if (i == numberOfRightAnswer){
+                        buttons.get(i).setText(names.get(numberOfQuestion));
+                    }else{
+                        int wrongAnswer = generateWrongAnswer();
+                        buttons.get(i).setText(names.get(wrongAnswer));
+                    }
+                }
+            }
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private  void generateQuestion(){
+        numberOfQuestion = (int)(Math.random() * names.size());
+        numberOfRightAnswer = (int) (Math.random() * buttons.size()) ;
+    }
+
+    private int generateWrongAnswer(){
+
+            int random = (int) (Math.random() * names.size());
+            while (random == numberOfQuestion) {
+                random = (int) (Math.random() * names.size());
+                generateWrongAnswer();
+            }
+            return random;
+
+
+    }
+
+    private void getContent(){
+        DownloadcontentTask content = new DownloadcontentTask();
+        try {
+            String urlContent = content.execute(urlSite).get();
+            String start = "<p class=\"link\">Topp 100 kändisar</p>";
+            String end = "<div class=\"col-xs-12 col-sm-6 col-md-4\">";
+            Pattern pattern = Pattern.compile(start+"(.*?)"+end);
+            Matcher matcher = pattern.matcher(urlContent);
+            String splitContent = "";
+            while (matcher.find()){
+                splitContent = matcher.group(1);
+            }
+
+            Pattern patternImg  = Pattern.compile("src=\"(.*?)\"");
+            Pattern patternName  = Pattern.compile("alt=\"(.*?)\"");
+            Matcher imgMatcher = patternImg.matcher(splitContent);
+            Matcher nameMatcher = patternName.matcher(splitContent);
+
+
+
+            while (imgMatcher.find()){
+                urls.add(imgMatcher.group(1));
+            }
+
+            while (nameMatcher.find()){
+                names.add(nameMatcher.group(1));
+            }
+
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void OnGuessClick(View view) {
+        Button button = (MaterialButton) view;
+        String  btText = button.getText().toString();
+
+        if (btText.equals(buttons.get(numberOfRightAnswer).getText().toString())){
+            Toast.makeText(this, "Right!", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Wrong! Right answer is: "+names.get(numberOfQuestion), Toast.LENGTH_SHORT).show();
+        }
+        playGame();
+    }
+
+    private static class  DownloadImages extends AsyncTask<String,Void,Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+
+            URL url = null;
+            HttpURLConnection httpURLConnection = null;
+
+
+            try {
+                url = new URL(strings[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = httpURLConnection.getInputStream();
+                return BitmapFactory.decodeStream(in);
+
+
+
+
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static  class DownloadcontentTask extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            URL url =null;
+            HttpURLConnection httpURLConnection = null;
+            StringBuilder stringBuilder = new StringBuilder();
+
+            try {
+                url = new URL(strings[0]);
+                httpURLConnection = (HttpURLConnection)url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                String line = bufferedReader.readLine();
+                while (line != null){
+                    stringBuilder.append(line);
+                    line = bufferedReader.readLine();
+                }
+
+                return stringBuilder.toString();
+
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
 }

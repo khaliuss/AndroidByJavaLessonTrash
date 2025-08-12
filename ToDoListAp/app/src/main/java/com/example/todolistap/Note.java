ad2352558e04beeab1680c03a0b65a3da638ee50
khaliuss <khalidkhushmadov@gmail.com>
2025-08-12 14:49:02 +0300

    Room learning  starts

--- ToDoListAp/app/src/main/java/com/example/todolistap/Note.java
+++ ToDoListAp/app/src/main/java/com/example/todolistap/Note.java
@@ -1,7 +1,12 @@
 package com.example.todolistap;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "notes")
 public class Note {
 
+    @PrimaryKey
     private int id;
     private int priority;
     private String text;
---
+++ ToDoListAp/app/src/main/java/com/example/todolistap/NotesDataBase.java
@@ -0,0 +1,27 @@
+package com.example.todolistap;
+
+import android.app.Application;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Note.class},version = 1)
+public abstract class NotesDataBase extends RoomDatabase {
+
+    private static NotesDataBase instance = null;
+    private static final String DB_NAME = "notes.db";
+
+    public static NotesDataBase getInstance(Application application){
+        if (instance == null){
+            instance = Room.databaseBuilder(
+                    application,
+                    NotesDataBase.class,
+                    DB_NAME
+                    ).build();
+        }
+        return instance;
+    }
+
+
+}
